359325c244a312bb6097150f4716bca577a81d41
josealberto <tizon.josealberto@gmail.com>
2026-01-26 20:02:06 -0300

    fix port

--- browser/voting_endpoints.js
+++ browser/voting_endpoints.js
@@ -381,4 +381,6 @@ app.post("/vote", async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("JS server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`JS server running on port ${PORT}`));
